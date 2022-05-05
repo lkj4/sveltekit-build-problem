52f84c3b4fcb90d3fb85d5bd25bb0c6b51d39dff
lkj4 <maxremy@protonmail.com>
2022-05-05 09:26:08 +0200

    rm env

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const client = await MongoClient.connect(
-		`mongodb://root:${<string>process.env['MONGO_INITDB_ROOT_PASSWORD']}@db_volume:27017`
+		`mongodb://root:dontputpasswordsintoyourcode@db_volume:27017`
 	);
 	return await resolve(event);
 };
