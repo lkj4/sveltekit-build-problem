4e5e79bb78153d3fddf8d9740efd91d788324b47
lkj4 <maxremy@protonmail.com>
2022-05-05 09:35:21 +0200

    add console.log

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,6 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 import { MongoClient } from 'mongodb';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	console.log('Why am I running???');
 	const client = await MongoClient.connect(
 		`mongodb://root:dontputpasswordsintoyourcode@db_volume:27017`
 	);
